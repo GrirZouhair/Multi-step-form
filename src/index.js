facd12e8ee727f8a524faffcd403d303cc98f4dd
Grir_Zouhiar <zouhairgrir937@gmail.com>
2025-03-15 21:42:32 +0000

    set-up the env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import StepOne from './Step_One/StepOne';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StepOne />
   </React.StrictMode>
 );
 
